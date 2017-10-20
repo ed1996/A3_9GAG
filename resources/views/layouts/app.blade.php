756cf15fa2b910c9c296a797fe2e8909954fe5dc
Unknown <maximerle@gmail.com>
2017-10-20 16:51:45 +0200

    boutons problème

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -205,17 +205,12 @@
             </a>
         </div>
         @auth
-<<<<<<< HEAD
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav navbar-right">
-                    
-=======
+
 
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="{{ route('article.index') }} class="icon-bar">Home</a></li>
->>>>>>> master
-                <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
+
+                <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Deconnexion</a></li>
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
 
