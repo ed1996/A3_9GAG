b54d6eda7206de6a4316670786f4a79eb2d28e6d
Unknown <maximerle@gmail.com>
2017-10-21 19:16:37 +0200

    Page index - mise en place des posts + bugs

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,66 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
+    @if(session('success'))
+        <div class="alert alert-success">
+            {{session('success')}}
+        </div>
+    @endif
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div style="text-align: center;"><h1>Fil d'actualité :</h1></div>
-                    <div class="panel-heading"> <a style="margin-left: 300px;" href="{{route('article.create')}}">Ajouter un Post</a></div>
+            @forelse($articles as $article)
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading" style="text-align: center;"><h3>{{ $article->title }}</h3></div>
 
-                    <div class="panel-body">
-
-                        @if(session('success'))
-                            <div class="alert alert-success">
-                                {{session('success')}}
-                            </div>
-                        @endif
-                        @forelse($articles as $article)
-                            <h3>{{ $article->title }}</h3>
-
+                        <div class="panel-body">
                             @if(!$article->picture)
                                 <img src="http://placehold.it/50x50"><br>
                             @else
                                 <img src="{{ asset('uploads/article_pictures/' . $article->picture) }}" alt=""><br>
                             @endif
 
-
+                            <p>Les personnes qui ont aimées ce post :
                                 @foreach ($article->likes as $user)
                                     {{ $user->name }} likes this !
                                 @endforeach
-
-                                @include('components.share', ['url' => route('article.show', ['id' => $article->id])])<br>
+                            </p>
+                            @include('components.share', ['url' => route('article.show', ['id' => $article->id])])<br>
 
 
 
                             {{ $article->likes->count() }} Likes<br>
 
-                                @if ($article->isLiked)
-                                    <a href="{{ route('article.like', $article->id) }}">Unlike</a><br>
-                                @else
-                                    <a href="{{ route('article.like', $article->id) }}">Like this!</a><br>
-                                @endif
+                            @if ($article->isLiked)
+                                <a href="{{ route('article.like', $article->id) }}">Unlike</a><br>
+                            @else
+                                <a href="{{ route('article.like', $article->id) }}">Like this!</a><br>
+                            @endif
 
                             <a href="{{route('article.show', ['id' => $article->id])}}">
-                                Voir ce Post
-                            </a>
 
-<<<<<<< Updated upstream
-=======
+                        </div>
 
-                                <hr>
-
->>>>>>> Stashed changes
-                        @empty
-                            Rien du tout
-                        @endforelse
                     </div>
-                    <div class="text-center">
-                        {{$articles->links()}}
-                    </div>
+
                 </div>
-
-            </div>
+            @empty
+                Rien du tout
+            @endforelse
         </div>
     </div>
 @endsection
--- resources/views/components/share.blade.php
+++ resources/views/components/share.blade.php
@@ -4,15 +4,15 @@
 <div class="social-buttons">
     <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($url) }}"
        target="_blank">
-        <img src="{{URL::asset('/images/facebook.png')}}">
+        <img src="{{URL::asset('/images/facebook.png')}}" style="height: 30px;width: 30px;">
     </a>
     <a href="https://twitter.com/intent/tweet?url={{ urlencode($url) }}"
        target="_blank">
-        <img src="{{URL::asset('/images/twitter.png')}}">
+        <img src="{{URL::asset('/images/twitter.png')}}" style="height: 30px;width: 30px;">
     </a>
     <a href="https://plus.google.com/share?url={{ urlencode($url) }}"
        target="_blank">
-        <img src="{{URL::asset('/images/google-plus.png')}}">
+        <img src="{{URL::asset('/images/google-plus.png')}}" style="height: 30px;width: 30px;">
     </a>
 </div>
 
